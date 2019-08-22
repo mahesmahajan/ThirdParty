d4d7a2d0e0cb8c4cb1c66cfa59c697d6a08c81b0
Maheshwar Mahajan <v-mahajm@microsoft.com>
2019-08-21 17:07:49 -0700

    WebView ThirdParty Join Changes

---
+++ renderer.js
@@ -0,0 +1,80 @@
+// inyector.js// Get the ipcRenderer of electron
+const { ipcRenderer } = require('electron');
+
+var videoEnable = false;
+
+ipcRenderer.on("bind-end-meeting", function (event, data) {
+    switch(data) {
+        case 'webex': loadWebexMeeting(); break;
+        case 'zoom':  loadZoomMeeting(); break;
+        default : console.log('channel name is missing'); break;
+    }
+});
+
+/**
+ * Simple function to excute the zoom meeting 
+ * of the <webview>
+ *
+ **/
+function loadZoomMeeting() {
+    
+    var meetingIdentifier = document.querySelector('.footer__leave-btn.ax-outline');
+    setTimeout(function(){
+        document.querySelectorAll(".left-tool-item button")[2] && document.querySelectorAll(".left-tool-item button")[2].click(); 
+    },10000);
+    if (meetingIdentifier) {
+        meetingIdentifier.addEventListener('click', function () {
+            setTimeout(function(){   
+            var endmeeting = document.querySelector('.zm-btn.zm-btn-legacy.zm-btn-primary');
+                endmeeting.addEventListener('click', function () { 
+                    ipcRenderer.sendToHost('close meeting');
+                });
+            },500);
+        });
+    }
+}
+/**
+ * Simple function to excute the webex meeting 
+ * of the <webview>
+ *
+ **/
+function loadWebexMeeting() {
+    setTimeout(function () {
+        var crossLink = document.querySelector('.pb-meeting-loader.pb-ani-home .loading-action .leave-meeting');
+        setTimeout(function () {
+           
+            var mainContainer = document.querySelector('#pb_iframecontainer');
+            if (mainContainer.childNodes[0]) {
+            
+                var video = mainContainer.childNodes[0].contentDocument.querySelectorAll('.menu-item.haspop.icon-video.item-preview.btn-52');
+                var red = mainContainer.childNodes[0].contentDocument.querySelectorAll('.menu-item.haspop.red.icon-leave.item-leave.btn-52');
+                video[0] && video[0].classList.remove('unavailable');
+                if(!videoEnable) {
+                    video[0].click();
+                    videoEnable = true;
+                
+                
+               
+                red[0] && red[0].click();
+                var startMeeting = mainContainer.childNodes[0].contentDocument.querySelectorAll('.button.green.start-my-video');
+                startMeeting[0].removeAttribute("disabled");
+                startMeeting[0] && startMeeting[0].click();
+                }
+
+                var leaveMeeting = mainContainer.childNodes[0].contentDocument.querySelectorAll('.button.leave-btn.first-item.last-item');
+                if (leaveMeeting.length > 0) {
+                    leaveMeeting[0].addEventListener('click', function () {
+                        ipcRenderer.sendToHost('close meeting');
+                    });
+                }
+            }
+        }, 5000);
+
+        if (crossLink) {
+            crossLink.addEventListener('click', function () {
+                ipcRenderer.sendToHost('close meeting webex');
+            });
+        }
+
+    }, 1000);
+}
